2aee8e18bf99ee94031c29fcb51215dc7ee91f34
Vidar Magnusson <git@vidarmagnusson.com>
2023-09-26 21:15:27 +0200

    Fuck do I know

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
     windows_subsystem = "windows"
 )]
 
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
 fn main() {
@@ -11,17 +12,30 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
- 
+
 #[derive(Deserialize)]
 enum Method {
     Get,
     Post,
     Put,
     Delete,
-    Option,
+    Options,
     Head,
 }
- 
+
+impl From<Method> for reqwest::Method {
+    fn from(m: Method) -> Self {
+        match m {
+            Method::Get => reqwest::Method::GET,
+            Method::Post => reqwest::Method::POST,
+            Method::Put => reqwest::Method::PUT,
+            Method::Delete => reqwest::Method::DELETE,
+            Method::Options => reqwest::Method::OPTIONS,
+            Method::Head => reqwest::Method::HEAD,
+        }
+    }
+}
+
 #[derive(Deserialize)]
 struct Request {
     url: String,
@@ -35,12 +49,23 @@ struct Response {
 
 #[derive(Serialize)]
 struct Status {
-    code: u8, 
-    text: String
+    code: u8,
+    text: String,
 }
 
 #[tauri::command]
-fn send_request(request: Request) -> Result<Response, String> {
-    let client = Client::builder().
+async fn send_request(request: Request) -> Result<Response, String> {
+    let client = Client::new();
+    let response = client
+        .request(request.method.into(), request.url)
+        .send()
+        .await
+        .map_err(String::from)?;
+
+    Ok(Response {
+        status: Status {
+            code: 200,
+            text: "OK".into(),
+        },
+    })
 }
- 
