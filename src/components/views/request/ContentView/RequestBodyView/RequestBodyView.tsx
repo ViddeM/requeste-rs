914fd2651c3b6bc4c7329c50b4157efcb6aa834d
Vidar Magnusson <git@vidarmagnusson.com>
2023-10-05 22:22:42 +0200

    Update to code editor

--- src/components/views/request/ContentView/RequestBodyView/RequestBodyView.tsx
+++ src/components/views/request/ContentView/RequestBodyView/RequestBodyView.tsx
@@ -2,7 +2,7 @@ import { Dropdown } from "@/components/elements/dropdown/Dropdown";
 import { TextArea } from "@/components/elements/input/TextArea";
 import { BodyTypes } from "@/types/request";
 import styles from "./RequestBodyView.module.scss";
-import { ReactElement, useRef, useState } from "react";
+import { Key, useState } from "react";
 import { Button } from "@/components/elements/button/Button";
 
 const ALL_BODY_TYPES: BodyTypes[] = [
@@ -12,7 +12,6 @@ const ALL_BODY_TYPES: BodyTypes[] = [
 ];
 
 export const RequestBodyView = () => {
-  const textAreaRef = useRef<ReactElement>(null);
   const [json, setJson] = useState<string>("");
   const rowCount = json.split("\n").length;
 
@@ -29,32 +28,18 @@ export const RequestBodyView = () => {
       <div className={styles.editorContainer}>
         <div className={styles.codeLinesContainer}>
           {Array.from(Array(rowCount).keys()).map((n) => (
-            <p>{`${n + 1}: `}</p>
+            <p key={n as Key}>{`${n + 1}: `}</p>
           ))}
         </div>
+        <div className={styles.divider} />
         <TextArea
           className={styles.codeTextArea}
           value={json}
-          ref={textAreaRef}
+          rows={1}
           onChange={(e) => setJson(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key !== "Tab") {
-              return;
-            }
-
-            e.preventDefault();
-
-            let start = textAreaRef.selectionStart;
-            let end = textAreaRef.selectionEnd;
-
-            textAreaRef.selectionStart = 0;
-            textAreaRef.selectionEnd = 4;
-
-            setJson(json.substring(0, start) + "  " + json.substring(end));
-          }}
         />
       </div>
-      <Button onClick={() => setJson(formatJson(json))}>Format</Button>
+      <Button onClick={() => setJson(formatJson(json))}>Format JSON</Button>
     </div>
   );
 };
